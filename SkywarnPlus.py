479fffca9e70a8c40f1cf5657015203c374f771b
frostyfruits <166942605+frostyfruits@users.noreply.github.com>
2026-03-16 18:10:46 -0400

    Update SkywarnPlus.py

--- SkywarnPlus.py
+++ SkywarnPlus.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/SkywarnPlus/venv/bin/python
+#!/usr/local/bin/SkywarnPlus/.venv/bin/python
 """
 SkywarnPlus.py v0.8.0 by Mason Nelson
 ===============================================================================
